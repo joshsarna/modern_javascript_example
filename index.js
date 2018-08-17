d20b8ddbf166800f955571209d90f8ec43f3d31d
joshsarna <joshsarna@gmail.com>
2018-08-16 19:22:53 -0700

    install webpack, webpack-dev-server, babel, and build tasks

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+var moment = require('moment');
+// import * as math from 'mathjs';
+// console.log(math.sqrt(-4));
+
 console.log("index.js");
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+var x = "four";
+console.log(`I have ${x} blueberries`)
